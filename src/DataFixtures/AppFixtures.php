8d8e176e05b7c22c6a25ef4ca669ee5bb2d5184a
JulienGrade <gradejulien@gmail.com>
2020-01-31 20:12:37 +0100

    Create the ManyToOne link between User and Ad and fill the bdd with faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Ad;
 use App\Entity\Image;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
@@ -15,6 +16,26 @@ class AppFixtures extends Fixture
 
         $faker = Factory::create('FR-fr');
 
+        // Nous gérons les utilisateurs
+
+        $users = [];
+        for($i = 1; $i <= 10; $i++)
+        {
+            $user = new User();
+
+            $user->setFirstName($faker->firstname)
+                ->setLastName($faker->lastname)
+                ->setEmail($faker->email)
+                ->setIntroduction($faker->sentence())
+                ->setDescription('<p>'.join('</p><p>',$faker->paragraphs(3)) . '</p>')
+                ->setHash('password');
+
+            $manager->persist($user);
+            $users[] = $user;
+        }
+
+
+        // Nous gérons les annonces
 
         for($i=1; $i <= 30; $i++)
         {
@@ -27,13 +48,16 @@ class AppFixtures extends Fixture
             $content = '<p>'.join('</p><p>',$faker->paragraphs(5)) . '</p>';
 
 
+            $user = $users[mt_rand(0, count($users) - 1)];
+
             $ad->setTitle($title)
 
                 ->setCoverImage($coverImage)
                 ->setIntroduction($introduction)
                 ->setContent($content)
                 ->setPrice(mt_rand(40, 200))
-                ->setRooms(mt_rand(1, 5));
+                ->setRooms(mt_rand(1, 5))
+                ->setAuthor($user);
 
             for($j = 1; $j <= mt_rand(2,5); $j++)
             {
