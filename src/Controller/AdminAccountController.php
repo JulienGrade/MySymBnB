a5c027d4a03d0226a9ed79e7fd3385a537b65bfe
JulienGrade <gradejulien@gmail.com>
2020-02-03 15:23:52 +0100

    create the /admin/login and /logout route, update security.yaml and links to logout

---
+++ src/Controller/AdminAccountController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class AdminAccountController extends AbstractController
+{
+    /**
+     * @Route("/admin/login", name="admin_account_login")
+     * @param AuthenticationUtils $utils
+     * @return Response
+     */
+    public function login(AuthenticationUtils $utils)
+    {
+        $error = $utils->getLastAuthenticationError();
+        // Permet de ne pas avoir a retaper son nom d'utilisateur
+        $username = $utils->getLastUsername();
+
+        return $this->render('admin/account/login.html.twig', [
+            'hasError' => $error !== null,
+            'username' => $username
+        ]);
+    }
+
+    /**
+     * Permet de se deconnecter de l'espace admin
+     *
+     * @Route("/admin/logout", name="admin_account_logout")
+     */
+    public function logout()
+    {
+
+    }
+}
