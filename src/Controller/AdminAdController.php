33caffdfeb89c9beb4031f11278cfb52dcadf872
JulienGrade <gradejulien@gmail.com>
2020-02-03 14:02:56 +0100

    Create the admin controller and /admin/ads index.html.twig with datas

---
+++ src/Controller/AdminAdController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\AdRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminAdController extends AbstractController
+{
+    /**
+     * @Route("/admin/ads", name="admin_ads_index")
+     * @param AdRepository $repo
+     * @return Response
+     */
+    public function index(AdRepository $repo)
+    {
+        return $this->render('admin/ad/index.html.twig', [
+            'ads' => $repo->findAll()
+        ]);
+    }
+}
