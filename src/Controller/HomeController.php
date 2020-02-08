1e4a54290fb126fb3406acd0aa71f5e56435d35a
JulienGrade <gradejulien@gmail.com>
2020-02-08 16:28:47 +0100

    Create the dashboard and StatsService, update /homepage with new request in repository and links

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\AdRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +15,14 @@ class HomeController extends AbstractController
      * Undocumented function
      *
      * @Route("/",name="homepage")
+     * @param AdRepository $adRepo
+     * @param UserRepository $userRepo
+     * @return Response
      */
-    public function home(){
-        return $this->render('home.html.twig');
+    public function home(AdRepository $adRepo, UserRepository $userRepo){
+        return $this->render('home.html.twig', [
+            'ads' => $adRepo->findBestAds(3),
+            'users' => $userRepo->findBestUsers(2)
+        ]);
     }
 }
